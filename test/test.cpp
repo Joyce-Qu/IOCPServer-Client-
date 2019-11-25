ca3701de42d0196281a7a9d0f1b96bcad6136a09
agaojs <a@gaojs.cn>
2019-11-25 09:35:12 +0800

    测试new 失败的情况

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void noMemoryToAlloc()
+{
+	cerr << "unable to satisfy request for memory\n";
+	abort();
+}
+
+int main()
+{
+	try 
+	{ 
+		//set过后，新的异常处理函数会被调用到
+		set_new_handler(noMemoryToAlloc);
+		size_t size = 0x7FFFFFFFFFFFFFFF;
+		int* p = new int[size];
+		if (p == 0) // 检查 p 是否空指针
+		{
+			return -1;
+		}
+	}
+	catch (const bad_alloc & e) 
+	{ 
+		//否则，会调用到这里
+		return -1;
+	}
+}
